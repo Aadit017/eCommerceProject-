16630dd69f93611671c785a0b18df43648de61e6
Aadit <baggaaadit017@gmail.com>
2021-03-17 19:00:45 +0530

    made some wireframe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import {HomePage} from "./homepage.component";
 import './App.css';
-
 function App() {
   return (
     <div className="App">
+    <HomePage />                  
     </div>
   );
 } 
-
 export default App;
---
+++ src/homepage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import "./homepage.styles.scss"
+export const HomePage= ()=>{
+    return (
+        <div className="homepage">
+            <div className="content">
+                <div className="directory-menu">
+                    <div className="menu-items">
+                    <div className="content">
+                           <h1 className="title">HATS</h1>
+                            <span className="subtitle">Shop</span>
+                        </div>
+                        </div>
+                <div className="menu-items">
+                    <div className="content">
+                        <h1 className="title">Jackets</h1>
+                        <span className="subtitle">Shop</span>
+                    </div>
+                </div>
+                <div className="menu-items">
+                    <div className="content">
+                        <h1 className="title">women</h1>
+                        <span className="subtitle">Shop</span>
+                    </div>
+                </div>
+                <div className="menu-items">
+                    <div className="content">
+                        <h1 className="title">men</h1>
+                        <span className="subtitle">Shop</span>
+                    </div>
+                </div>
+                <div className="menu-items">
+                    <div className="content">
+                        <h1 className="title">sneakers</h1>
+                        <span className="subtitle">Shop</span>
+                    </div>
+                </div>
+                </div>  
+            </div>
+        </div>
+    )
+}
+
