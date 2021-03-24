66f04320fc31465cb42366f136b0c6aa9184e300
Aadit <baggaaadit017@gmail.com>
2021-03-24 10:45:02 +0530

    added the image to the collection preview

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -7,8 +7,7 @@ const CollectionItem =({id,name,price,imageUrl}) =>{
             <div 
             className="image"
             style={{
-                backgroundImage:`url${{imageUrl}}`,
-
+                backgroundImage:`url(${imageUrl})`,
             }}
              
             />
