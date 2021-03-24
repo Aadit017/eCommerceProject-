524251a9ac4d8a7bc835b1c2b11a818451f174aa
Aadit <baggaaadit017@gmail.com>
2021-03-24 12:14:28 +0530

    30 %

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{ useState, useEffect } from 'react';
 import {HomePage} from "./pages/homepage/homepage.component"
 import {Route} from "react-router-dom"
 import './App.css';
