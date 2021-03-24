02396219ae6903770a44f4f13d1f38dc36750342
Aadit <baggaaadit017@gmail.com>
2021-03-24 13:52:02 +0530

    watch sign in thingy again

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,13 +2,31 @@ import React, { Component } from "react"
 import "./sign-in-and-sign-up.style.scss"
 
 class SignIn extends Component{ 
-    constructor(){
-        super();
-
+    constructor(props){
+        super(props);
+        this.state={
+            email:'',
+            password: '' 
+        }
+    }
+    handleSubmit=event=>{
+        event.preventDefault();
+        this.setState({email:' ',password:' '})
     }
 render(){
     return(
-        <div></div>
+        <div className="sign-in" >
+            <h2>I already have an account </h2>
+            <span> sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+            <input type="email" name="email" value={this.state.email} placeholder="enter your email" required />
+            <label>Email</label>
+            <input type="password" name="password" value={this.state.password} placeholder="enter your password" required />
+            <label>Password</label>
+            <input type="submit" value="Submit-form" />
+        </form>
+        </div>
     )
 }
 }
+export default SignIn
