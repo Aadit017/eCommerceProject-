cd19a6ef5ab2a2e13a3411cf1bf703c2386ff53d
Aadit <baggaaadit017@gmail.com>
2021-04-06 18:49:40 +0530

    now adding the root reducer file

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
+import { Provider } from 'react-redux';
+
+
 
 ReactDOM.render(
+  <Provider>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>
+  ,
+
 document.getElementById('root')
 );
