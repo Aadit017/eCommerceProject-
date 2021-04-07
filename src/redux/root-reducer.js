4f84ece5de5fe703ab504319429fa5129b78ce87
Aadit <baggaaadit017@gmail.com>
2021-04-07 17:48:59 +0530

    made the rooot reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,1 +1,7 @@
 // overall reduxer store 
+import { combineReducers } from "redux";
+import userReducer from './user/user.reducer';
+
+export default combineReducers({
+    user :userReducer
+})
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { applyMiddleware, createStore } from "redux";
+
+
+
+
+
+
+
+
